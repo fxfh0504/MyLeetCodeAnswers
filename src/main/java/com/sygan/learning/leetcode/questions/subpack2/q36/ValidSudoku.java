10095be345e31cc71b5ef86561eed2c6e8a4a51c
sygan <sygan@Ctrip.com>
2018-04-20 17:59:32 +0800

    commit

---
+++ src/main/java/com/sygan/learning/leetcode/questions/subpack2/q36/ValidSudoku.java
@@ -0,0 +1,14 @@
+package com.sygan.learning.leetcode.questions.subpack2.q36;
+
+/**
+ * @Description:
+ * @author: ganshiyu
+ * @Date: 2018-04-19
+ * @Time: 17:09
+ */
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+
+        return true;
+    }
+}
