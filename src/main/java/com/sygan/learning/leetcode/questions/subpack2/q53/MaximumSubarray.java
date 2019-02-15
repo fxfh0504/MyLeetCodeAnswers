95c7f4e59936c34ee2199be02db0bcb77f2f0203
sygan <531623249@qq.com>
2019-02-15 22:38:39 +0800

    coding

--- src/main/java/com/sygan/learning/leetcode/questions/subpack2/q53/MaximumSubarray.java
+++ src/main/java/com/sygan/learning/leetcode/questions/subpack2/q53/MaximumSubarray.java
@@ -1,4 +1,36 @@
 package com.sygan.learning.leetcode.questions.subpack2.q53;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ */
 public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0)
+                sum += num;
+            else
+                sum = num;
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+    public static void main(String...args){
+        int[] ints = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int i = maxSubArray(ints);
+        System.out.println(i);
+    }
 }
