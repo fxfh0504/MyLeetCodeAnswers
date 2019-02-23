27f6eeca30c3edb49feae7b918ff288ff4c48361
sygan <531623249@qq.com>
2019-02-24 01:17:30 +0800

    123

--- src/main/java/com/sygan/learning/leetcode/questions/subpack2/q56/MergeIntervals.java
+++ src/main/java/com/sygan/learning/leetcode/questions/subpack2/q56/MergeIntervals.java
@@ -1,17 +1,52 @@
 package com.sygan.learning.leetcode.questions.subpack2.q56;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MergeIntervals {
-    public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals,(o1,o2)->o1.start-o2.start);
-        return intervals;
+    public static void main(String...args){
+        ArrayList<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1,4));
+        intervals.add(new Interval(4,5));
+//        intervals.add(new Interval(8,10));
+//        intervals.add(new Interval(15,18));
+        merge(intervals);
+    }
+    public static List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size()==0){
+            return intervals;
+        }
+        Collections.sort(intervals,(o1,o2)->{
+            if (o1.start-o2.start==0){
+                return o1.end-o2.end;
+            }else {
+                return o1.start-o2.start;
+            }
+        });
+        List<Interval> result=new ArrayList<>();
+        Interval temp=intervals.get(0);
+        for (int i=0;i<intervals.size();i++){
+            Interval curr = intervals.get(i);
+            if (curr.start<=temp.end){
+                if (curr.end>temp.end){
+                    temp.end=curr.end;
+                }
+            }else {
+                result.add(temp);
+                temp=curr;
+
+            }
+            if (i==intervals.size()-1){
+                result.add(temp);
+            }
+        }
+        return result;
     }
 
 
-    public class Interval {
+    public static class Interval {
         int start;
         int end;
 
