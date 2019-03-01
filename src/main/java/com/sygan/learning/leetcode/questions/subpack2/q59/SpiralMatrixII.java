9624392adf348419b36edcbeeaefabaab6b6a39f
shiyu.gsy <shiyu.gsy@alibaba-inc.com>
2019-03-01 23:59:48 +0800

    Create SpiralMatrixII.java

---
+++ src/main/java/com/sygan/learning/leetcode/questions/subpack2/q59/SpiralMatrixII.java
@@ -0,0 +1,43 @@
+package com.sygan.learning.leetcode.questions.subpack2.q59;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: shiyu.gan
+ * @create: 2019-03-01 23:53
+ **/
+
+/**
+ * Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 8, 9, 4 ],
+ *  [ 7, 6, 5 ]
+ * ]
+ */
+public class SpiralMatrixII {
+    public int[][] generateMatrix(int n) {
+        int[][] arr = new int[n][n];
+        int c = 1, j = 0;
+        while (c <= n * n) {
+
+            for (int i = j; i < n - j; i++)
+                arr[j][i] = c++;
+            for (int i = j + 1; i < n - j; i++)
+                arr[i][n - j - 1] = c++;
+            for (int i = n - j - 2; i >= j; i--)
+                arr[n - j - 1][i] = c++;
+            for (int i = n -j - 2; i > j; i--)
+                arr[i][j] = c++;
+
+            j++;
+        }
+
+        return arr;
+    }
+}
